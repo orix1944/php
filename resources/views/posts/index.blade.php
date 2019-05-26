6e3c9dff11d3bfa49f2e06fd6cb37ce05f61aa6a
orix1944 <bf109.sai@gmail.com>
2019-05-26 17:44:13 +0900

    ll

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,14 +12,12 @@
     @foreach($posts as $post)
         <div class="card">
           <div class="card-body">
+            <h5 class="card-title">{{ $post->title }}</h5>
             <h5 class="card-title">
-
                 カテゴリー:{{ $post->category->category_name }}</h5>
             <h5 class="card-title">
-
                 投稿者:{{ $post->user->name }}</h5>
-            <h5 class="card-title">{{ $post->title }}</h5>
-            <p class="card-text">{{ $post->contenbt }}</p>
+            <p class="card-text">{{ $post->content }}</p>
             <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細</a>
           </div>
         </div>
