4d469cf7b5cd809c78dad64751ded3959d8fe90d
orix1944 <bf109.sai@gmail.com>
2019-05-28 19:53:47 +0900

    feat:remove-some-foreignkey

--- database/migrations/2019_05_20_061401_create_comments_table.php
+++ database/migrations/2019_05_20_061401_create_comments_table.php
@@ -23,7 +23,7 @@ class CreateCommentsTable extends Migration
 
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            // $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
